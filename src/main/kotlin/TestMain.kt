6be536a61c152315966e73195afce146adaa76d2
Sergey Babakov <67116260+BF-Brus-Li@users.noreply.github.com>
2022-04-27 00:16:36 +0300

    Delete TestMain.kt

--- src/main/kotlin/TestMain.kt
+++
@@ -1,9 +0,0 @@
-import java.io.File
-
-private fun main() {
-    val readListFromFile = File("input")
-        .readLines()
-    println("\t${readListFromFile.joinToString(" \n\t")}")
-    tasksList += readListFromFile
-    println(tasksList.joinToString(" "))
-}
